9ea4f7a924ce7342730aa265a7e22f3d89e62cd2
Jonathan Joestar <35832815+BR-Liu@users.noreply.github.com>
2020-03-27 15:51:01 +0800

    Update TestServiceHealthChecker.java

--- CountDownLatch/TestServiceHealthChecker.java
+++ CountDownLatch/TestServiceHealthChecker.java
@@ -3,7 +3,6 @@ package TestThread.TestCountDown;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -18,16 +17,21 @@ public class TestServiceHealthChecker {
 
     private static boolean allServiceIsHealth = true;
 
+    private final static int LATCH_COUNT = 2;
+
     public static void main(String[] args) throws Exception {
-        HealthCheckerCountDownLatch healthCheckerCountDownLatch = new HealthCheckerCountDownLatch(2);
+        HealthCheckerCountDownLatch healthCheckerCountDownLatch = new HealthCheckerCountDownLatch(LATCH_COUNT);
 
         services.add(new RedisServiceHealthChecker(healthCheckerCountDownLatch));
         services.add(new DBServiceHealthChecker(healthCheckerCountDownLatch));
 
-        Executor executor = new ThreadPoolExecutor(3,
-                3, 10, TimeUnit.SECONDS,
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 3,
+                10, TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(1),
                 new ThreadPoolExecutor.AbortPolicy());
+        //允许核心线程数过期
+        executor.allowCoreThreadTimeOut(true);
+
         for (ServiceBaseHealthChecker serviceBaseHealthChecker : services) {
             executor.execute(serviceBaseHealthChecker);
         }
@@ -39,6 +43,6 @@ public class TestServiceHealthChecker {
         }
 
         System.out.println("全部服务启动情况: " + allServiceIsHealth);
-
+        executor.shutdown();
     }
 }
