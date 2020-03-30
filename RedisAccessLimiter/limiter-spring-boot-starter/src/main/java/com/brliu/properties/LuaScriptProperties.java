a5095ab1feb09e5280fb426b08f528c48c983727
Jonathan Joestar <35832815+BR-Liu@users.noreply.github.com>
2020-03-30 16:28:26 +0800

    删除lombok依赖，减小组件依赖

--- RedisAccessLimiter/limiter-spring-boot-starter/src/main/java/com/brliu/properties/LuaScriptProperties.java
+++ RedisAccessLimiter/limiter-spring-boot-starter/src/main/java/com/brliu/properties/LuaScriptProperties.java
@@ -1,13 +1,34 @@
 package com.brliu.properties;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
 @ConfigurationProperties(prefix = "access.limit")
 public class LuaScriptProperties {
 
     private String classPath = "/script/";
     private String fileName = "rateLimiter.lua";
 
+    public LuaScriptProperties() {
+    }
+
+    public LuaScriptProperties(String classPath, String fileName) {
+        this.classPath = classPath;
+        this.fileName = fileName;
+    }
+
+    public String getClassPath() {
+        return classPath;
+    }
+
+    public void setClassPath(String classPath) {
+        this.classPath = classPath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
